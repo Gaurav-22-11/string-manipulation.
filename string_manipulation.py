45b5977cc7527b170b05edf5ea9e35605d03e3eb
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2024-03-13 14:06:56 +0530

    Create string_manipulation.py

---
+++ string_manipulation.py
@@ -0,0 +1,9 @@
+class StringManipulation:
+  def concatenate(self,str1,str2):
+    return str1 + " " + str2
+
+  def convert_to_upper(self,string):
+    return string.upper()
+
+  def extract_substring(self,string,start,end):
+    return string[start:end]
