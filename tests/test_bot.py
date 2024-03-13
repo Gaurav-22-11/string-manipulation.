5d854d553a23503c2113f04c48d7158ef8a2571e
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2024-03-13 14:19:24 +0530

    Update test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,2 +1,21 @@
 import unittest
-from
+from string_manipulation import StringManipulation
+
+class TestStringManipulation(unittest.TestCase)
+  def setUp():
+    self.bot=StringManipulation()
+
+  def test_concatenate(self):
+    result=self.bot.concatenate("Hello","World")
+    self.assertEqual(result, "Hello World")
+
+  def test_uppercase_conversion(self):
+    result= self.bot.convert_to_upper("hello")
+    self.assertEqual(result,"HELLO")
+
+  def test_substring_extraction(self):
+    result= self.bot.extract_substring("Hello","World!",7,12)
+    self.assertEqual(result, "World")
+
+if __name__ == '__main__':
+  unittest.main()
